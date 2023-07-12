ecb596f832a754b47720cf46495703aa2a42bffb
MehmetYogun <mehmetyogun90@gmail.com>
2023-07-12 16:27:52 +0300

    create SDP offer

---
+++ main.js
@@ -0,0 +1,45 @@
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"],
+    },
+  ],
+};
+
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById("user-1").srcObject = localStream;
+
+  createOffer();
+};
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection();
+
+  remoteStream = new MediaStream();
+  document.getElementById("user-2").srcObject = remoteStream;
+
+  localStream.getTracks().forEach((tracks) => {
+    peerConnection.addTrack(tracks, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((tracks) => {
+      remoteStream.addTrack();
+    });
+  };
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  console.log("offer: ", offer);
+};
+
+init();
